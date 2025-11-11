5a993457ee3a0c821a1a708bff880a23e1511db3
alinurd <ali.mnrdn14005@gamail.com>
2025-11-12 03:42:46 +0700

    ok

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div class="min-h-[220px] flex flex-col sm:justify-center items-center px-15  py-20 bg-white">
     <div>
         {{ $logo }}
     </div>
