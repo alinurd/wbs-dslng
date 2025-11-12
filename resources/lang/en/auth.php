ee575c01a24e3a2a6c6203c9ac48a26360b520b5
alinurd <ali.mnrdn14005@gamail.com>
2025-11-12 17:52:40 +0700

    add lang auth

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+      'login' => 'Log in',
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Remember Me',
+    'forgot_password' => 'Forgot Your Password?',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'register' => 'Register',
+    'name' => 'Full Name',
+    'confirm_password' => 'Confirm Password',
+    'reset_password' => 'Reset Password',
+    'send_reset_link' => 'Send Password Reset Link',
+    'already_have_account' => 'Already have an account?',
+    'dont_have_account' => "Don't have an account?",
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+];
---
+++ resources/lang/id/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'login' => 'Masuk',
+    'email' => 'Email',
+    'password' => 'Kata Sandi',
+    'remember_me' => 'Ingat Saya',
+    'forgot_password' => 'Lupa Kata Sandi?',
+    'login' => 'Masuk',
+    'logout' => 'Keluar',
+    'register' => 'Daftar',
+    'name' => 'Nama Lengkap',
+    'confirm_password' => 'Konfirmasi Kata Sandi',
+    'reset_password' => 'Atur Ulang Kata Sandi',
+    'send_reset_link' => 'Kirim Tautan Atur Ulang Kata Sandi',
+    'already_have_account' => 'Sudah punya akun?',
+    'dont_have_account' => 'Belum punya akun?',
+    'failed' => 'Kredensial yang Anda masukkan tidak cocok dengan catatan kami.',
+    'throttle' => 'Terlalu banyak percobaan masuk. Silakan coba lagi dalam :seconds detik.',
+];
