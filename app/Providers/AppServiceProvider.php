79cc65b379fc3a5cb91fc7b48ed8b92f8ee3c63e
alinurd <ali.mnrdn14005@gamail.com>
2026-02-10 09:36:02 +0700

    add locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,7 @@ use App\Models\Menu;
 use App\Services\EmailService;
 use App\Services\MenuService;
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Session; 
 use Illuminate\Support\Facades\View;
 
 use Illuminate\Support\ServiceProvider;
@@ -80,11 +80,11 @@ class AppServiceProvider extends ServiceProvider
                 })
                 // Filter parent menu sesuai akses
                 ->filter(fn($menu) => $hasMenuAccess($menu));
-                $locale = Session::get('locale', config('app.locale'));
+                $locale = Session::get('locale', config('app.locale')); 
             // Bagikan ke semua view
             $view->with([
                 'menus' => $menus,
-                'locale' => $locale,
+                'locale' => $locale, 
                'user' => $user,
                'userRole' => $user?$user->roles()->get()->pluck('name', 'id')->toArray():[],
                 'module_permissions' => module_permissions('dashboard'),
